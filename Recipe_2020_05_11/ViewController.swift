0be05155b50ca7277ae0a50fa82c30344d356736
helloconey <48701450+helloconey@users.noreply.github.com>
2020-05-11 21:53:51 -0700

    Initial Commit

---
+++ Recipe_2020_05_11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Recipe_2020_05_11
+//
+//  Created by Coney Lu on 5/11/20.
+//  Copyright © 2020 Hua Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
